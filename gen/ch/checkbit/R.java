8244e0345fdfed18c6ae6098a11da53d49e7c93b
Zla <zlatka.mihaylova@googlemail.com>
2013-06-09 12:28:44 +0200

    unused stuff deleted

--- gen/ch/checkbit/R.java
+++ gen/ch/checkbit/R.java
@@ -62,11 +62,10 @@ public final class R {
         public static final int water=0x7f0a0001;
     }
     public static final class layout {
-        public static final int activity_cut=0x7f030000;
-        public static final int activity_grow=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_move=0x7f030003;
-        public static final int activity_music=0x7f030004;
+        public static final int activity_grow=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_move=0x7f030002;
+        public static final int activity_music=0x7f030003;
     }
     public static final class menu {
         public static final int cut=0x7f090000;
